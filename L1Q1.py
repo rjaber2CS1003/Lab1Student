daf5172e9621e49dc7058c86358ad412f622e47a
Scott Bateman <scottb@unb.ca>
2021-09-07 14:05:39 -0300

    updated the lab files.

--- L1Q1.py
+++ L1Q1.py
@@ -1,2 +1,5 @@
-# Student Name, Student Number
+# Your Student Name, Your Student Number
+# Your Partner's Name, Your Partner's Number
+# Your Other Partner's Name, Your Other Partner's Number (delete if no other partner)
+
 # Write your code for Question 1 below
